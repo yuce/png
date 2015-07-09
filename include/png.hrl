1ed30f4f91021868922bddb698e888e43537f091
Yuce Tekol <yucetekol@gmail.com>
2015-07-09 22:19:38 +0300

    Trivial

--- include/png.hrl
+++ include/png.hrl
@@ -6,9 +6,9 @@
 -define(PNG_COLOR_RGBA, 6).
 
 -record(png_config, {width = 0,
-               height = 0,
-               bit_depth = 8,
-               color_type = 0,
-               compression_method = 0,
-               filter_method = 0,
-               interlace_method = 0}).
+                     height = 0,
+                     bit_depth = 8,
+                     color_type = 0,
+                     compression_method = 0,
+                     filter_method = 0,
+                     interlace_method = 0}).
